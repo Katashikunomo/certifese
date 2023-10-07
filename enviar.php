d3375fa650f3b64732aff4084aeeeaf6201a2aa7
Andres Malfavaun <2203024384@alumnos.cua.uam.mx>
2023-10-06 23:49:35 -0600

    se modifico el redireccionamiento de correo

--- enviar.php
+++ enviar.php
@@ -1,19 +1,36 @@
 <?php
-$nombre = $_POST['nombre'];
-$email = $_POST['correo'];
-$asunto = 'Formulario Rellenado de CERTIFICACIONES';
-$mensaje = "Nombre: ".$nombre."\nEmail: $email \nMensaje:".$_POST['mensaje'];
-$mail = @mail('francisco@fese.mx', $asunto, $mensaje);
 
-if($mail){
-    echo "Correo enviado";
-    sleep(1);
-    header("Location: index.html");
-}
-else 
-{
-    header("Location: index.html");
-}
+$destinatario = 'alexis@fese.mx';
+$nombre = $_POST['nombre'];
+$asunto = 'Formulario Rellenado de CERTIFICACIONES : '.$_POST['asunto'];
+$email = $_POST['correo'];
+
+$header = "Enviado desde Entidad Certificadora";
+$mensaje = "Nombre: ".$nombre."\nEmail: $email \nMensaje:".$_POST['mensaje'];
+
+
+mail($destinatario, $asunto, $mensaje, $header);
+
+
+echo"<script>alert('correo enviado exitosamente')</script>";
+echo"<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+
+// if($mail){
+//     echo "Correo enviado";
+//     // sleep(0.1);
+//     header("Location: index.html");
+// }
+// else 
+// {
+//     // header("Location: index.html");
+
+//     ?>
+<!-- //     <script> -->
+<!-- //         window.location = "index.html" -->
+<!-- //     </script> -->
+//     <?php 
+
+// }
 //echo "Correo enviado";
 //sleep(1);
 
